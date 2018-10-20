f39fe22691b196975c3cac47dbfe3d91a6709089
Donald Desloge <thedesloge@gmail.com>
2018-10-19 21:51:00 -0700

    Instantiate quill_rat

--- adventure_time.py
+++ adventure_time.py
@@ -3,7 +3,12 @@ from mobs import quill_rat, zombie
 
 damage = 10
 health = 100
+#You want to instantiate the mob/enemy here like below
+# mob = quill_rat()
+
 def attack(mob):
+    # then do something like this
+    # mob.print_details()
     print(quill_rat.print_details)
 
 def display_intro():
