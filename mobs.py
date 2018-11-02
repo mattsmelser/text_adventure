beef7e4a7ca6d906b7f976526ed3fc0efb423cba
Donald Desloge <thedesloge@gmail.com>
2018-11-02 12:27:51 -0700

    Update mobs.py

--- mobs.py
+++ mobs.py
@@ -1,13 +1,17 @@
 #enemies list
 import random
 
-class quill_rat():
+class QuillRat():
     #mob = quill_rat()
     #mob.print_details()
     health = 40
     attack = random.randint(0,16)
     name = "Quill Rat"
     attack1 = "bite"
-    #def print_details(quill_rat):
-       #print("You encounter a {}.  It has {} health and it attacks you with {} doing {} damage.".format(name, health, attack1, attack)
+
+
+    def print_details(self):
+       print("You encounter a {}.  It has {} health and it attacks you with {} doing {} damage.".format(self.name, self.health, self.attack1, self.attack))
+
+
 
