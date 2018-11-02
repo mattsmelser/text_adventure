b0632c452055bb0baf01f63912267a0771f60ba4
Matt <MattSmelser2007@gmail.com>
2018-11-02 13:53:21 -0600

    created monster generator

--- adventure_time.py
+++ adventure_time.py
@@ -1,10 +1,11 @@
 import random, time, sys
-from mobs import QuillRat#, zombie
+from mobs import QuillRat, Zombie, MonsterGenerator
 
 damage = 10
 health = 100
 #You want to instantiate the mob/enemy here like below
-mob = QuillRat()  #Instantiate the quill_rat class, now mob is a quill_rat
+mob = MonsterGenerator()
+mob.generate_list()
 
 def attack(mob):
     # then do something like this
@@ -25,4 +26,6 @@ def character():
 print(start)
 #print(quill_rat.print_details())
 
-attack(mob)
+attack(mob.pick_monster())
+
+#create list and randomly select from the list to choose which monster to fight
--- mobs.py
+++ mobs.py
@@ -8,10 +8,25 @@ class QuillRat():
     attack = random.randint(0,16)
     name = "Quill Rat"
     attack1 = "bite"
-
-
     def print_details(self):
        print("You encounter a {}.  It has {} health and it attacks you with {} doing {} damage.".format(self.name, self.health, self.attack1, self.attack))
 
 
+class Zombie():
+    #mob = quill_rat()
+    #mob.print_details()
+    health = 40
+    attack = random.randint(0,16)
+    name = "Zombie"
+    attack1 = "smack"
+    def print_details(self):
+       print("You encounter a {}.  It has {} health and it attacks you with {} doing {} damage.".format(self.name, self.health, self.attack1, self.attack))
 
+class MonsterGenerator():
+    def __init__(self):
+        self.monster_list = []
+    def generate_list(self):
+        self.monster_list.append(QuillRat()) #manually adding monsters to monster list
+        self.monster_list.append(Zombie())
+    def pick_monster(self):
+        return self.monster_list[random.randint(0,len(self.monster_list))] #randomly picks monster from above list
