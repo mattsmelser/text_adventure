6b9e03004498df91e79a225309afde3e54fef3ed
Donald Desloge <thedesloge@gmail.com>
2018-11-04 21:22:45 -0800

    Updates to mobs.py

--- mobs.py
+++ mobs.py
@@ -6,6 +6,7 @@ class QuillRat():
     #mob.print_details()
     health = 40
     attack = random.randint(0,16)
+    location = "swamp"
     name = "Quill Rat"
     attack1 = "bite"
     def print_details(self):
@@ -17,6 +18,7 @@ class Zombie():
     #mob.print_details()
     health = 40
     attack = random.randint(0,16)
+    location = "cave"
     name = "Zombie"
     attack1 = "smack"
     def print_details(self):
@@ -25,8 +27,24 @@ class Zombie():
 class MonsterGenerator():
     def __init__(self):
         self.monster_list = []
+        self.location_map = {}
     def generate_list(self):
         self.monster_list.append(QuillRat()) #manually adding monsters to monster list
         self.monster_list.append(Zombie())
-    def pick_monster(self):
+
+    def generate_location_list(self):
+        self.location_map["cave"] = Zombie()
+        self.location_map["swamp"] = QuillRat()
+
+    def pick_monster1(self):
         return self.monster_list[random.randint(0,len(self.monster_list))] #randomly picks monster from above list
+
+    def pick_monster2(self, location):
+        output = []
+        for monster in self.monster_list:
+          if monster.location == location:
+            output.append(monster)
+
+        return output
+
+        return self.location_map[location]
